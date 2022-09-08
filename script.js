6dca6fa242f1fcef95b40b3dd4703f56c87763e7
Matth-s <108790362+Matth-s@users.noreply.github.com>
2022-09-08 10:09:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const menu = document.querySelector('#container-header');
 const body = document.querySelector('body');
 const toggle = document.querySelector('.toggle');
-const test = document.querySelector('.test')
+const test = document.querySelector('.test');
 
 toggle.addEventListener('click', function() {
     body.classList.toggle('scale');
-    test.classList.toggle('background')
+    test.classList.toggle('background');
 });
 
 const sous_menu1 = document.querySelector('#un');
@@ -16,18 +16,26 @@ const lien = document.querySelectorAll('.menu');
 
 lien.forEach((item) => {
     item.addEventListener('click', function(e) {
-        console.log('click lien')
 
         switch (e.target.id) {
             case "menu-un": 
                 fleche1.classList.toggle('rotation');
-                sous_menu1.classList.toggle('display-block')
+                sous_menu1.classList.toggle('display-block');
                 break;
             case "menu-deux": 
                 fleche2.classList.toggle('rotation');
-                sous_menu2.classList.toggle('display-block')
+                sous_menu2.classList.toggle('display-block');
                 break;
         }
     });
 });
 
+fleche1.addEventListener('click', function() {
+    fleche1.classList.toggle('rotation');
+    sous_menu1.classList.toggle('display-block');
+});
+
+fleche2.addEventListener('click', function() {
+    fleche2.classList.toggle('rotation');
+    sous_menu2.classList.toggle('display-block');
+});
